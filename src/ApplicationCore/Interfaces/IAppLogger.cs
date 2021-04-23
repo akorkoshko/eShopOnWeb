9215cb077a372cb4206bf9e6bb451c9f09cbb1e8
akorkoshko <66247438+akorkoshko@users.noreply.github.com>
2021-04-23 15:14:35 +0300

    Update IAppLogger.cs

--- src/ApplicationCore/Interfaces/IAppLogger.cs
+++ src/ApplicationCore/Interfaces/IAppLogger.cs
@@ -1,7 +1,7 @@
 ﻿namespace Microsoft.eShopWeb.ApplicationCore.Interfaces
 {
     /// <summary>
-    /// This type eliminates the need to depend directly on the ASP.NET Core logging types.
+    /// This type eliminates the need to depend directly on the ASP.NET Core logging types. Add some comments
     /// </summary>
     /// <typeparam name="T"></typeparam>
     public interface IAppLogger<T>
